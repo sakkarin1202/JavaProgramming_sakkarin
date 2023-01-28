70b7293d6c53807abfc7f942f78c248c8ab97015
sakkarin1202 <xfake12340@hotmail.com>
2023-01-28 13:39:53 +0700

    Update

--- Exercise2/Exercise26.java
+++ Exercise2/Exercise26.java
@@ -6,8 +6,8 @@ Scanner scan = new Scanner(System.in);
 float a = scan.nextFloat();
 float b = scan.nextFloat();
 double sum = (a-b)*(a+b);
-System.out.println(“A =?” + a);
-System.out.println(“B =?” + b);
-System.out.println(“(“ + a + “ - “ + b + “)” + “(“ + a + “ + “ + b + “)” + “ = “ + sum);
+System.out.println("A =?" + a);
+System.out.println("B =?" + b);
+System.out.println("(" + a + " - " + b + ")" + "(" + a + " + " + b + ")" + " = " + sum);
 }
 }
