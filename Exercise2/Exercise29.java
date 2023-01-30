130ac608dc35e208329027207908788f5dfd8a41
sakkarin1202 <xfake12340@hotmail.com>
2023-01-30 11:17:23 +0700

    update

--- Exercise2/Exercise29.java
+++ Exercise2/Exercise29.java
@@ -6,11 +6,11 @@ Scanner scan = new Scanner(System.in);
 float usd = scan.nextFloat();
 double rate = 35.50;
 double sum = usd*rate;
-System.out.print(“Enter Dollar (USD) amount : “);
-System.out.printf(“%.2f”, usd);
+System.out.print("Enter Dollar (USD) amount : ");
+System.out.printf("%.2f", usd);
 System.out.println();
-System.out.printf(“Exchange rate 1 (USD) = %.2f (THB)”, rate);
+System.out.printf("Exchange rate 1 (USD) = %.2f (THB)", rate);
 System.out.println();
-System.out.printf(“ %.2f (USD) = %.2f (THB)”, usd, sum);
+System.out.printf("%.2f (USD) = %.2f (THB)", usd, sum);
 }
 }
