04cd968b633d6a5e24295cd2607909f19a259abb
sakkarin1202 <xfake12340@hotmail.com>
2023-02-17 11:51:26 +0700

    update

--- Exercise3/Exercise36.java
+++ Exercise3/Exercise36.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Exercise36 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
         double sum = 1.0;
         for (int i = 1; i <= 100; i++) {
             if (i % 2 == 0) {
